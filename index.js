80209121237012986668778bd632498529c18b66
kairuz <evan.kairuz@gmail.com>
2023-06-25 21:14:18 -0400

    Added initial implementation.

---
+++ index.js
@@ -0,0 +1,307 @@
+import {Context, SEED_PARAM_NAME, DEFAULT_OPS_PER_ITER, OPS_PER_ITER_PARAM_NAME,
+  ROAD_HEIGHT_NODES, ROUGH_SIZE_NODES, NODE_SIZE, GRID_HEIGHT, GRID_WIDTH} from "./context.js";
+import {SeededRandom} from "./utils.js";
+
+
+window.addEventListener('load', () => {
+
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const seedParam = urlSearchParams.has(SEED_PARAM_NAME) ? urlSearchParams.get(SEED_PARAM_NAME) : null;
+  const seedVal = seedParam !== null && Number.isInteger(Number(seedParam)) ? Number(seedParam) : Math.trunc(Math.random() * 10000); // DEFAULT_SEED_VAL;
+  console.log(`seedVal=${seedVal}`);
+  const opsPerIterParam = urlSearchParams.has(OPS_PER_ITER_PARAM_NAME) ? urlSearchParams.get(OPS_PER_ITER_PARAM_NAME) : null;
+  const opsPerIter = opsPerIterParam !== null && Number.isInteger(Number(opsPerIterParam)) ? Number(opsPerIterParam) : DEFAULT_OPS_PER_ITER;
+
+  const context = Context(SeededRandom(seedVal));
+  const {grid, road, roughLeftTopIjs} = context;
+
+
+  const findPathWorker = new Worker(new URL(`./find-path-worker.js` +
+                                            `?${SEED_PARAM_NAME}=${seedVal}` +
+                                            `&${OPS_PER_ITER_PARAM_NAME}=${opsPerIter}`,
+                                            import.meta.url), {type: 'module'});
+  findPathWorker.postMessage('wake up');
+
+  // const PATH_COLORS = ['lightgreen', 'orange', 'lightblue']; //, 'pink', 'yellow', 'sandybrown', 'midnightblue', 'cornflowerblue'];
+  const PATH_COLORS = ['lightgreen', 'orange', 'lightblue', 'pink', 'yellow', 'sandybrown', 'midnightblue', 'cornflowerblue'];
+
+  const canvas = document.createElement('canvas');
+  document.body.appendChild(canvas);
+  canvas.style.position = 'absolute';
+  canvas.height = GRID_HEIGHT;
+  canvas.width = GRID_WIDTH;
+  const canvasContext = canvas.getContext('2d');
+
+  const pathsAnimatorCanvas = document.createElement('canvas');
+  document.body.appendChild(pathsAnimatorCanvas);
+  pathsAnimatorCanvas.style.position = 'absolute';
+  pathsAnimatorCanvas.height = GRID_HEIGHT;
+  pathsAnimatorCanvas.width = GRID_WIDTH;
+
+  const frontCanvas = pathsAnimatorCanvas;
+
+  const pathsAnimatorCanvasOffscreen = pathsAnimatorCanvas.transferControlToOffscreen();
+
+  const pathsAnimatorWorker = new Worker(new URL(`./paths-animator-worker.js?${SEED_PARAM_NAME}=${seedVal}`, import.meta.url), {type: 'module'});
+  console.log('pathsAnimatorWorker.postMessage');
+  pathsAnimatorWorker.postMessage({canvas: pathsAnimatorCanvasOffscreen}, [pathsAnimatorCanvasOffscreen]);
+
+  let pathIdSeq = 0;
+  const pathsMax = PATH_COLORS.length;
+  const paths = new Map();
+  const pathsNodesColors = new Map();
+
+  const deletePath = (pathId) => {
+    const path = paths.get(pathId);
+    paths.delete(pathId);
+    const prevColor = PATH_COLORS[pathId % pathsMax];
+    path.forEach((pathNode) => {
+      const colorList = pathsNodesColors.get(pathNode);
+      colorList.delete(prevColor);
+      if (colorList.size === 0) {
+        pathsNodesColors.delete(pathNode);
+      }
+    });
+    pathsAnimatorWorker.postMessage({removePath: pathId});
+  };
+
+  const addPath = (pathNodeIjs) => {
+    const pathNodes = Array(pathNodeIjs.length);
+    const pathNodeIjsReversed = Array(pathNodeIjs.length);
+
+    for (let i = pathNodeIjs.length - 1; i >= 0; i--) {
+      const [pathNodeIjI, pathNodeIjJ] = pathNodeIjs[i];
+      pathNodes[i] = context.grid.nodeAt(pathNodeIjI, pathNodeIjJ);
+      pathNodeIjsReversed[(pathNodeIjs.length - 1) - i] = pathNodeIjs[i];
+    }
+
+    const newPathId = pathIdSeq++;
+
+    if (paths.size === pathsMax) {
+      const pathOldestId = newPathId - pathsMax;
+      deletePath(pathOldestId);
+    }
+    paths.set(newPathId, pathNodes);
+    const color = PATH_COLORS[newPathId % pathsMax];
+
+    pathNodes.forEach((pathNode) => {
+      if (pathsNodesColors.has(pathNode)) {
+        pathsNodesColors.get(pathNode).add(color);
+      }
+      else {
+        pathsNodesColors.set(pathNode, new Set().add(color))
+      }
+    });
+    console.log('pathsAnimatorWorker.postMessage({addPath: [newPathId, pathNodeIjs, color]});');
+    pathsAnimatorWorker.postMessage({addPath: [newPathId, pathNodeIjsReversed, color]});
+
+  };
+
+  const clearPaths = () => {
+    pathsAnimatorWorker.postMessage('clearPaths');
+    paths.clear();
+    pathsNodesColors.clear();
+  };
+
+  findPathWorker.addEventListener('message', (e) => {
+    const data = e.data;
+    let ind = 0;
+
+    const startTime = data[ind++];
+    const [startNodeI, startNodeJ] = data[ind++];
+    const _startNode = context.grid.nodeAt(startNodeI, startNodeJ);
+    const [targetNodeI, targetNodeJ] = data[ind++];
+    const _targetNode = context.grid.nodeAt(targetNodeI, targetNodeJ);
+    const success = data[ind++];
+    const workerTimeTakenMillis = data[ind++];
+    const endTime = performance.now();
+    const timeTakenMillis = endTime - startTime;
+    const roundTripTimeTaken = endTime - startTime;
+    console.log(`timeTakenMillis=${timeTakenMillis.toFixed(2)}, workerTimeTakenMillis=${workerTimeTakenMillis.toFixed(2)} round trip timeTaken=${roundTripTimeTaken.toFixed(2)}`);
+
+    if (success === true) {
+      const pathNodeIjs = data[ind++];
+      const hasDuplicates = data[ind++];
+      (hasDuplicates ? data[ind++].concat(startTime) : [startTime]).forEach((_startTime) => {
+        addPath(pathNodeIjs);
+        drawGrid(_startNode, _targetNode);
+      });
+      console.log(`findPath took ${performance.now() - startTime} ms`);
+    }
+  });
+
+
+  let startNode = null;
+  let targetNode = null;
+
+  const findPathStart = (_startNode, _targetNode) => {
+    const startTime = performance.now();
+    const message = [
+        startTime,
+        [_startNode.i, _startNode.j],
+        [_targetNode.i, _targetNode.j]
+    ];
+    findPathWorker.postMessage(message);
+  };
+
+
+  const drawGrid = (_startNode, _targetNode) => {
+    canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+    grid.obstacles.forEach((obstacle, i) => {
+      canvasContext.beginPath();
+      canvasContext.fillStyle = 'black';
+      canvasContext.fillRect(obstacle.leftPos, obstacle.topPos, obstacle.width, obstacle.height);
+      canvasContext.closePath();
+    });
+
+    const walkablePaddingPerc = 10;
+    const walkablePadding = (grid.nodeSize * walkablePaddingPerc / 100);
+    const unwalkablePaddingPerc = 13.3333333;
+    const unwalkablePadding = (grid.nodeSize * unwalkablePaddingPerc / 100);
+
+    for (let i = 0; i < grid.nodesPerRow; i++) {
+      for (let j = 0; j < grid.nodesPerCol; j++) {
+        const node = grid.nodeAt(i, j);
+        canvasContext.beginPath();
+        if (node.walkable) {
+          canvasContext.strokeStyle = 'lightgrey';
+          canvasContext.strokeRect(i * grid.nodeSize + walkablePadding, j * grid.nodeSize + walkablePadding,
+                             grid.nodeSize - (walkablePadding * 2), grid.nodeSize - (walkablePadding * 2));
+        }
+        else {
+          canvasContext.fillStyle = 'crimson';
+          canvasContext.fillRect(i * grid.nodeSize + unwalkablePadding, j * grid.nodeSize + unwalkablePadding,
+                           grid.nodeSize - (unwalkablePadding * 2), grid.nodeSize - (unwalkablePadding * 2));
+        }
+        canvasContext.closePath();
+      }
+    }
+
+    {
+      canvasContext.beginPath();
+      canvasContext.fillStyle = 'slategrey';
+
+      for (let i = 0; i < road.length; i += ROAD_HEIGHT_NODES) {
+        const roadNode = road[i];
+        canvasContext.fillRect(roadNode.i * NODE_SIZE, roadNode.j * NODE_SIZE, NODE_SIZE, NODE_SIZE * ROAD_HEIGHT_NODES);
+      }
+      canvasContext.closePath();
+    }
+
+    {
+      canvasContext.beginPath();
+      canvasContext.fillStyle = 'olive';
+      for (let i = 0; i < roughLeftTopIjs.length; i++) {
+        const roughLeftTopIj = roughLeftTopIjs[i];
+        canvasContext.fillRect(roughLeftTopIj.i * NODE_SIZE, roughLeftTopIj.j * NODE_SIZE, ROUGH_SIZE_NODES * NODE_SIZE, ROUGH_SIZE_NODES * NODE_SIZE);
+      }
+      canvasContext.closePath();
+    }
+
+    paths.forEach((path) => {
+      if (path === null) {
+        return;
+      }
+      for (let i = 0; i < path.length; i++) {
+        const pathNode = path[i];
+        const pathNodeColors = pathsNodesColors.get(pathNode);
+        let iii = 0;
+        for (const color of pathNodeColors) {
+          const ii = iii;
+          iii++;
+
+          canvasContext.beginPath();
+
+          const paddingFactor = 0.1;
+          const padding = (grid.nodeSize * paddingFactor);
+          canvasContext.fillStyle = color;
+
+          canvasContext.fillRect(
+              pathNode.leftPos + padding + ((grid.nodeSize - (2 * padding)) / pathNodeColors.size) * ii,
+              pathNode.topPos + padding,
+              (grid.nodeSize - (2 * padding)) / pathNodeColors.size,
+              grid.nodeSize - 2 * padding
+          );
+          canvasContext.closePath();
+
+        }
+      }
+    });
+
+    if (_startNode !== null) {
+      canvasContext.beginPath();
+      canvasContext.fillStyle = 'blue';
+      const startNodePaddingPerc = 10;
+      const startNodePadding = (grid.nodeSize * startNodePaddingPerc / 100);
+      const halfNodeSize = grid.nodeSize / 2;
+      canvasContext.fillRect(_startNode.posX - halfNodeSize + startNodePadding, _startNode.posY - halfNodeSize + startNodePadding,
+                       grid.nodeSize - (startNodePadding * 2), grid.nodeSize - (startNodePadding * 2));
+      canvasContext.strokeStyle = 'black';
+      canvasContext.strokeRect(_startNode.posX - halfNodeSize, _startNode.posY - halfNodeSize, grid.nodeSize, grid.nodeSize);
+      canvasContext.closePath();
+    }
+
+    if (_targetNode !== null) {
+      canvasContext.beginPath();
+      canvasContext.fillStyle = 'green';
+      const startNodePaddingPerc = 10;
+      const startNodePadding = (grid.nodeSize * startNodePaddingPerc / 100);
+      const halfNodeSize = grid.nodeSize / 2;
+      canvasContext.fillRect(_targetNode.posX - halfNodeSize + startNodePadding, _targetNode.posY - halfNodeSize + startNodePadding,
+                       grid.nodeSize - (startNodePadding * 2), grid.nodeSize - (startNodePadding * 2));
+      canvasContext.strokeStyle = 'black';
+      canvasContext.strokeRect(_targetNode.posX - halfNodeSize, _targetNode.posY - halfNodeSize, grid.nodeSize, grid.nodeSize);
+      canvasContext.closePath();
+    }
+  };
+
+  drawGrid(null, null);
+
+  frontCanvas.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+
+    const {pageX, pageY} = event;
+    const nodeClickedI = Math.trunc(pageX / grid.nodeSize);
+    const nodeClickedJ = Math.trunc(pageY / grid.nodeSize);
+    if (nodeClickedI >= grid.nodesPerRow || nodeClickedJ >= grid.nodesPerCol) {
+      return;
+    }
+    const _startNode = grid.nodeAt(nodeClickedI, nodeClickedJ);
+
+    if (!_startNode.walkable) {
+      return;
+    }
+
+    clearPaths();
+
+    startNode = _startNode;
+
+    drawGrid(_startNode, targetNode);
+    if (targetNode !== null) {
+      findPathStart(_startNode, targetNode);
+    }
+  });
+
+  frontCanvas.addEventListener('click', (event) => {
+
+    const {pageX, pageY} = event;
+    const nodeClickedI = Math.trunc(pageX / grid.nodeSize);
+    const nodeClickedJ = Math.trunc(pageY / grid.nodeSize);
+    if (nodeClickedI >= grid.nodesPerRow || nodeClickedJ >= grid.nodesPerCol) {
+      return;
+    }
+    const _targetNode = grid.nodeAt(nodeClickedI, nodeClickedJ);
+
+    if (!_targetNode.walkable) {
+      return;
+    }
+    targetNode = _targetNode;
+
+    drawGrid(startNode, _targetNode);
+    if (startNode !== null) {
+      findPathStart(startNode, _targetNode);
+    }
+  });
+
+});
---
+++ utils.js
@@ -0,0 +1,232 @@
+
+
+const randomInt = (max, randomNum = Math.random) => {
+  if (!Number.isInteger(max) || max < 0) {
+    throw 'invalid max';
+  }
+
+  return Math.trunc(randomNum() * max);
+
+};
+
+const randomChoice = (arr, randomNum = Math.random) => {
+  if (!Array.isArray(arr) || arr.length === 0) {
+    throw 'invalid arr';
+  }
+  return arr[randomInt(arr.length, randomNum)];
+};
+const randomChance = (outOf, randomNum = Math.random) => randomInt(outOf, randomNum) === 0;
+
+
+const seed = function(s) {
+  const mask = 0xffffffff;
+  let m_w  = (123456789 + s) & mask;
+  let m_z  = (987654321 - s) & mask;
+
+  return function() {
+    m_z = (36969 * (m_z & 65535) + (m_z >>> 16)) & mask;
+    m_w = (18000 * (m_w & 65535) + (m_w >>> 16)) & mask;
+
+    let result = ((m_z << 16) + (m_w & 65535)) >>> 0;
+    result /= 4294967296;
+    return result;
+  }
+};
+
+const DEFAULT_SEED_VAL = 1234;
+
+const SeededRandom = (seedVal = DEFAULT_SEED_VAL) => {
+
+  const randomNum = seed(seedVal);
+
+  return {
+    get seedVal(){return seedVal;},
+    num: () => randomNum(),
+    int: (max) => randomInt(max, randomNum),
+    choice: (arr) => randomChoice(arr, randomNum),
+    chance: (outOf) => randomChance(outOf, randomNum)
+  }
+
+};
+
+const DEFAULT_SEEDED_RANDOM = SeededRandom(DEFAULT_SEED_VAL);
+
+const NodeMinHeap = (heapIndexes, costs) => {
+  const arr = [];
+
+  const isLessThan = (nodeA, nodeB) => {
+    const costsA = costs.get(nodeA);
+    const costsB = costs.get(nodeB);
+    const fCostDiff = costsA.fCost - costsB.fCost;
+    return fCostDiff === 0 ? costsA.hCost - costsB.hCost < 0 : fCostDiff < 0;
+  };
+
+  const add = (node) => {
+    arr.push(node);
+    const heapIndex = arr.length - 1;
+    heapIndexes.set(node, heapIndex);
+    if (arr.length > 1) {
+      trickleUp(heapIndex);
+    }
+  };
+
+  const trickleUp = (index) => {
+    const node = arr[index];
+    const parentInd = Math.floor((index - 1) / 2);
+    const parent = arr[parentInd];
+
+    if (parentInd >= 0) {
+      const violation = isLessThan(node, parent);
+      if (violation) {
+        arr[parentInd] = node;
+        heapIndexes.set(node, parentInd);
+        arr[index] = parent;
+        heapIndexes.set(parent, index);
+        trickleUp(parentInd);
+      }
+    }
+  };
+
+  const peek = () => {
+    if (arr.length === 0) {
+      throw 'heap is empty';
+    }
+    return arr[0];
+  };
+
+  const pop = () => {
+    if (arr.length === 0) {
+      throw 'heap is empty';
+    }
+    else if (arr.length === 1) {
+      heapIndexes.delete(arr[0]);
+      return arr.pop();
+    }
+    const popped = arr[0];
+    heapIndexes.delete(popped);
+    arr[0] = arr.pop();
+    heapIndexes.set(arr[0], 0);
+    trickleDown(0);
+    return popped;
+  };
+
+  const trickleDown = (index) => {
+    const node = arr[index];
+    const child1Index = (index * 2) + 1;
+
+    const hasChild1 = child1Index in arr;
+
+    if (hasChild1) {
+      const child2Index = (index * 2) + 2;
+      const childNode1 = arr[child1Index];
+      const hasChild2 = child2Index in arr;
+      const childNode2 = hasChild2 ? arr[child2Index] : null;
+
+      const chosenChildIndex = hasChild2 && isLessThan(childNode2, childNode1) ?
+                               child2Index : child1Index;
+      const chosenChildNode = arr[chosenChildIndex];
+
+      const violation = isLessThan(chosenChildNode, node);
+      if (violation) {
+        arr[index] = chosenChildNode;
+        heapIndexes.set(chosenChildNode, index);
+        arr[chosenChildIndex] = node;
+        heapIndexes.set(node, chosenChildIndex);
+        trickleDown(chosenChildIndex);
+      }
+    }
+  };
+
+  return {
+    add,
+    peek,
+    pop,
+    updateNode: (node) => trickleUp(heapIndexes.get(node)),
+    contains: (node) => arr[heapIndexes.get(node)] === node,
+    clear: () => arr.length = 0,
+    get isEmpty(){return arr.length === 0;},
+    get isNotEmpty() {return !this.isEmpty;},
+    get size() {return arr.length;},
+    toArray: () => [...arr],
+    values: () => arr.values()
+  };
+
+};
+
+
+const degToRad = (degrees) => degrees * (Math.PI / 180);
+
+const radToDeg = (radians) => radians * (180 / Math.PI);
+
+const sinr = (radians) => Math.sin(radians);
+const sind = (degrees) => sinr(degToRad(degrees));
+const cosr = (radians) => Math.cos(radians);
+const cosd = (degrees) => cosr(degToRad(degrees));
+const tanr = (radians) => Math.tan(radians);
+const tand = (degrees) => tanr(degToRad(degrees));
+
+const atan2r = (y, x) => Math.atan2(y, x);
+const atan2d = (y, x) => radToDeg(atan2r(y, x));
+
+
+const Vec2 = (_x, _y) => {
+  let x = _x;
+  let y = _y;
+
+  const addXy = (_x, _y) => {x += _x; y += _y;};
+  const addScalar = (_n) => {x += _n; y += _n;};
+  const add = (_vec2) => addXy(_vec2.x, _vec2.y);
+
+  const mulXy = (_x, _y) => {x *= _x; y *= _y;};
+  const mul = (_vec2) => mulXy(_vec2.x, _vec2.y);
+  const mulScalar = (_scalar) => mulXy(_scalar, _scalar);
+
+  return {
+    toString: () => `[${x},${y}]`,
+    toPrettyStr: () => `[${x.toFixed(2)},${y.toFixed(2)}]`,
+    setXy: (_x = x, _y = y) => {x = _x; y = _y;},
+    set(that){Vec2.set(this, that);},
+    clear: () => {x = 0; y = 0;},
+    setFromArr: (_arr) => {x = _arr[0]; y = _arr[1];},
+    get x(){return x;},
+    get y(){return y;},
+    set x(_x){x = _x;},
+    set y(_y){y = _y;},
+    addXy,
+    add,
+    sub: (_vec2) => {x -= _vec2.x; y -= _vec2.y;},
+    addX: (_n) => x += _n,
+    addY: (_n) => y += _n,
+    diff: (_vec2) => Vec2(x - _vec2.x, y - _vec2.y),
+    normalize: () => {
+      const sqrtAdded = Math.sqrt(x**2 + y**2);
+      x /= sqrtAdded; y /= sqrtAdded;
+    },
+    mulXy,
+    mul,
+    mulScalar,
+    pow: (_n) => Vec2(Math.pow(x, _n), Math.pow(y, _n)),
+    sq: function(){return this.pow(2);},
+    toInv: () => Vec2(-x, -y),
+    setInv: () => {x = -x; y = -y;},
+    equals: (_vec2) => x === _vec2.x && y === _vec2.y,
+    copyOf: () => Vec2(x, y),
+    toArr: () => [x, y]
+  };
+};
+Vec2.copyOf = (_vec2) => _vec2.copyOf();
+Vec2.fromArr = (_arr) => Vec2(_arr[0], _arr[1], _arr[2]);
+Vec2.toArr = (_vec2) => _vec2.toArr();
+Vec2.set = (thiz, that) => {thiz.x = that.x; thiz.y = that.y;};
+Vec2.zero = () => Vec2(0, 0, 0);
+const _Vec2_zero = Vec2.zero();
+Vec2.zeroArr = () => _Vec2_zero.toArr();
+
+
+export {
+  DEFAULT_SEED_VAL, DEFAULT_SEEDED_RANDOM,
+  randomInt, randomChoice, randomChance, seed,
+  SeededRandom, NodeMinHeap,
+  degToRad, radToDeg, sinr, sind, cosr, cosd, tanr, tand, atan2r, atan2d,
+  Vec2
+};
